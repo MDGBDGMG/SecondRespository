9059ee4d305ea4e64e7daf6e387e54087fbcbc7f
xuanfucheng <243927522@qq.com>
2018-12-05 18:39:50 +0800

    人生在世屈指算，一共三万六千天

--- bigdata/src/main/java/Test1.java
+++ bigdata/src/main/java/Test1.java
@@ -3,5 +3,6 @@ public class Test1 {
         System.out.println("送你一句MMP");
         System.out.println("人生如戏");
         System.out.println("坎坷人生路");
+        System.out.println("人生在世屈指算，一共三万六千天");
     }
 }
